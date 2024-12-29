a7e6d8b1a2f99b1fc57bef984a1dc1d8d5f9870d
augustqa <augustqabatch@gmail.com>
2024-12-29 07:22:42 +0530

    Update UploadFile Techlearn.java
    Added Techlearn.in

--- src/fileupload/UploadFile.java
+++ src/fileupload/UploadFile.java
@@ -13,7 +13,7 @@ public class UploadFile {
 	public void attachfile() throws InterruptedException {
 		driver.get("https://www.seleniumlearn.com/registration/");
 		Thread.sleep(1000);
-		driver.findElement(By.xpath("//textarea[@id='textarea-23104650921']")).sendKeys("Hello Text Message");
+		driver.findElement(By.xpath("//textarea[@id='textarea-23104650921']")).sendKeys("Hello Techlearn.in");
 		Thread.sleep(1000);
 		driver.findElement(By.xpath("//*[@id=\"field-ioa3T81OSsEXwhP\"]")).sendKeys("C:\\Users\\kpuru\\OneDrive\\Documents\\Book2.xlsx");
 		driver.findElement(By.xpath("ABC")).click();
